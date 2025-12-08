1317b31e974f58569156189a9bc2abac5058b554
Alessandro Masci <s308579@studenti.polito.it>
2025-12-08 12:17:52 +0100

    posthog tracing

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,15 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { TranslationProvider } from './i18n/TranslationProvider'
+import posthog from 'posthog-js'
+
+posthog.init('phc_B3NBaN3DFIywYW6JBKoc62Bp552jTxhYwtl18g7vmy8',
+    {
+        api_host: 'https://eu.i.posthog.com',
+        person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+        autocapture: true, // enable automatic capture of pageviews and clicks
+    }
+)
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
